eb3a46d58163467f10dda0b75f8b12f8265dff5c
Mizuki Sango <msageha@gmail.com>
2017-12-19 17:41:23 +0900

    edit bag

--- src/readable_counter.py
+++ src/readable_counter.py
@@ -8,6 +8,8 @@ def translate(node):
     word = node.surface
     feature = node.feature.split(',')
     if feature[2] == '助数詞' and word_is_num(node.prev.surface) and data.counter_suffix_dicts.uncertainty_counter_suffix_dict.get(word):
+        if node.prev.prev and node.prev.prev.surface == '.' and node.prev.prev.prev and word_is_num(node.prev.prev.prev.surface):
+            return ','.join(feature), ','.join(feature) != node.feature
         num = int(node.prev.surface)
         counter_suffix_pronounce_dict = data.counter_suffix_dicts.uncertainty_counter_suffix_dict[word]
         if not (counter_suffix_pronounce_dict.get(num) or counter_suffix_pronounce_dict['only_under10']):
