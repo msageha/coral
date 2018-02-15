32771f409cb27eee75659830de107eef959ed862
Mizuki Sango <msageha@gmail.com>
2018-02-15 16:51:39 +0900

    add links

---
+++ dict/link_dicts.py
@@ -0,0 +1,5 @@
+'''link_dicts file'''
+
+link_dict = {'twitter.com': 'ツイッター', 'facebook.com': 'フェイスブック', 'github.com': 'ギットハブ', 'www.google': 'グーグル',
+    'www.yahoo': 'ヤフー', 'line.me': 'ライン', 'www.rakuten': '楽天', 'wikipedia.org': 'ウィキペディア', 'www.amazon': 'アマゾン',
+    'www.youtube': 'ユーチューブ', 'www.nicovideo': 'ニコニコ動画'}
